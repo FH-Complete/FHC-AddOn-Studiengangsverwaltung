e514bb8e348b10b63c896078c434c9ab6323f23b
alex <karpen_ko@hotmail.com>
2017-08-28 12:11:05 +0200

    Added edit option for Qualifikationsziele

--- vilesci/js/angular/controllers/studienordnung/sto-qualifikationsziele-controller.js
+++ vilesci/js/angular/controllers/studienordnung/sto-qualifikationsziele-controller.js
@@ -6,11 +6,15 @@ angular.module('stgv2')
 			ctrl.data = new Qualifikationsziel();
 			ctrl.data.studienordnung_id = $scope.studienordnung_id;
 			ctrl.temp = [];
+			ctrl.inputids = ["input_1", "input_2"];
 			
 			//enable tooltips after ngRepeat finished
 			$scope.$on("ngRepeatFinished",function(){
 				$('[data-toggle="tooltip"]').tooltip();
 			});
+
+			//hide edit buttons by default
+			$(".editButton").hide();
 
 			$http({
 				method: 'GET',
@@ -23,11 +27,11 @@ angular.module('stgv2')
 						ctrl.data = response.data.info[0];
 						$(ctrl.data.data[1].elements[1]).each(function(key, value)
 						{
-							ctrl.drawListItem('zielList_1', value);
+							ctrl.drawListItem('zielList_1', value, 'input_1');
 						});
 						$(ctrl.data.data[1].elements[2]).each(function(key, value)
 						{
-							ctrl.drawListItem('zielList_2', value);
+							ctrl.drawListItem('zielList_2', value, 'input_2');
 						});
 					}
 				}
@@ -40,7 +44,7 @@ angular.module('stgv2')
 			});
 
 			ctrl.save = function () {
-				var saveData = {data: ""}
+				var saveData = {data: ""};
 				ctrl.parseJson();
 				saveData.data = angular.copy(ctrl.data);
 				saveData.data.data = JSON.stringify(saveData.data.data);
@@ -66,12 +70,12 @@ angular.module('stgv2')
 				});
 			};
 
-			ctrl.drawListItem = function (div_id, text)
+			ctrl.drawListItem = function (div_id, text, input_id)
 			{
 				if(text!="")
 				{
 					var list = $("#"+div_id);
-					var html = $(list).append('<li class="list-group-item">'+text+'<span class="badge" ng-click="ctrl.removeListItem($event)"><span class="glyphicon glyphicon-trash"></span></span></li>');
+					var html = $(list).append('<li class="list-group-item">'+text+'<span class="badge" ng-click="ctrl.removeListItem($event)"><span class="glyphicon glyphicon-trash"></span></span><span class="badge" ng-click="ctrl.editListItem($event, \''+input_id+'\')"><span class="glyphicon glyphicon-edit"></span></span></li>');
 					$compile(html)(scope);
 				}
 			};
@@ -81,9 +85,46 @@ angular.module('stgv2')
 				var value = $("#"+input_id).val();
 				if(value!="")
 				{
-					ctrl.drawListItem(list_id, value);
+					ctrl.drawListItem(list_id, value, input_id);
 					$("#"+input_id).val("");
 					ctrl.save();
+				}
+			};
+
+			ctrl.editListItem = function (event, input_id)
+			{
+				ctrl.inputids.forEach(
+					function(entry){
+						ctrl.toggleEditVisibility($("#"+entry), false);
+					}
+				);
+				ctrl.toggleEditVisibility($("#"+input_id), true);
+				var editedItem = $(event.currentTarget).parent();
+				$("#"+input_id).val(editedItem.text());
+				ctrl.editedItem = editedItem;
+			};
+
+			ctrl.saveEditedListItem = function (input_id) {
+				ctrl.editedItem.contents().each(
+					function () {
+						if (this.nodeType === 3) {//3 = textnode
+							this.nodeValue = $("#" + input_id).val();
+							return false;
+						}
+					}
+				);
+				ctrl.toggleEditVisibility($("#"+input_id), false);
+				ctrl.save();
+			};
+
+			ctrl.toggleEditVisibility = function (input_element, visible){
+				if(visible) {
+					input_element.parent().find('span').hide();//hide buttons for adding
+					input_element.parent().find('.editButton').show();
+				}else{
+					input_element.parent().find('span').show();//show buttons for adding
+					input_element.parent().find('.editButton').hide();
+					input_element.val("");
 				}
 			};
 
