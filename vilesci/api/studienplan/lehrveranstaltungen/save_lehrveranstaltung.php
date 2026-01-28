d8f69a39aec83a89c4735d983145ae3a3d2163ab
Andreas Österreicher <oesi@technikum-wien.at>
2026-01-28 13:18:38 +0100

    Module können nun auch Lehrgängen oder Abteilungen zugeordnet werden

--- vilesci/api/studienplan/lehrveranstaltungen/save_lehrveranstaltung.php
+++ vilesci/api/studienplan/lehrveranstaltungen/save_lehrveranstaltung.php
@@ -126,7 +126,8 @@ function mapDataToLehrveranstaltung($data)
     $lv->benotung = parseBoolean($data->benotung);
     $lv->lvinfo = parseBoolean($data->lvinfo);
     $lv->lehrauftrag = parseBoolean($data->lehrauftrag);
-    $lv->lehrveranstaltung_template_id = $data->lehrveranstaltung_template_id;
+    if(isset($data->lehrveranstaltung_template_id))
+        $lv->lehrveranstaltung_template_id = $data->lehrveranstaltung_template_id;
     $lv->evaluierung = parseBoolean($data->evaluierung);
 
     return $lv;
